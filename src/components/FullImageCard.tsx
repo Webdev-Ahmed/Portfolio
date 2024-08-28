fd0ce9b8d79364a8b7c0b2fbe4fcf3ede404e259
Webdev-Ahmed <webdevahmedraza@gmail.com>
2024-08-28 08:52:47 -0700

    Some more minor fixes

--- src/components/FullImageCard.tsx
+++ src/components/FullImageCard.tsx
@@ -9,7 +9,7 @@ const FullImageCard = ({
 }) => {
   return (
     <div
-      className={`${className} w-full h-[700px] max-990:h-[clamp(470px,65vw,640px)] max-768:h-[clamp(640px,130vw,1064px)] max-480:h-[clamp(460px,130vw,640px)] bg-neutral-800/70 border-[1px] p-2 border-neutral-100/20 backdrop-blur-3xl rounded-3xl overflow-hidden shadow-center-md shadow-neutral-950/50`}
+      className={`${className} w-full h-[700px] max-990:h-[clamp(470px,65vw,640px)] max-768:h-[clamp(640px,130vw,1064px)] max-480:h-[clamp(460px,130vw,640px)] bg-neutral-800/35 border-[1px] p-2 border-neutral-100/20 rounded-3xl overflow-hidden shadow-center-md shadow-neutral-950/50`}
     >
       <div className="rounded-2xl border-[1px] overflow-hidden border-neutral-100/30 w-full h-full">
         <img
