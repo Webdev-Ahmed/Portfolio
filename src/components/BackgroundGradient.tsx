9e06e144f01c58678936481ccb7e3b6e8f2fc68b
Webdev-Ahmed <webdevahmedraza@gmail.com>
2024-08-28 07:48:51 -0700

    Lessen the blur size

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -2,7 +2,7 @@ const BackgroundGradient = ({ className }: { className: string }) => {
   return (
     <>
       <div className="absolute w-[100%] h-[100vh] pointer-events-none flex justify-center -z-[99]">
-        <div className="absolute w-screen h-screen pointer-events-none backdrop-blur-[15vh]"></div>
+        <div className="absolute w-screen h-screen pointer-events-none backdrop-blur-[100px]"></div>
         <div
           className={`${className} absolute pointer-events-none -z-[99] w-[80vw] h-[65vh] rounded-t-none rounded-full`}
         ></div>
